db963afdacc83c1d456fac95ebcb70f9c13097ad
VTStudio <vin20777@gmail.com>
2020-10-17 16:16:57 -0700

    Deploy a JSON API

--- my-app/index.js
+++ my-app/index.js
@@ -1,12 +1,29 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
+const linkedInLink =  { name: "LinkedIn", url: "https://www.linkedin.com/in/yu-ting-tsao-a68262a2/"}
+const appsLink =  { name: "App Store", url: "https://apps.apple.com/us/developer/yu-ting-tsao/id1328112477"}
+const githubLink =  { name: "Github", url: "https://github.com/vin20777/cloudflare-2020-general-engineering-assignment"}
+const links = [linkedInLink, appsLink, githubLink]
+const linksJSONResponse = links.map((link) => JSON.stringify(link))
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
+
+  const url = new URL(request.url)
+  var urlPath = url.pathname
+
+  if (urlPath == "/links") {
+    return new Response(linksJSONResponse, {
+      headers: { 'content-type': 'application/json;charset=UTF-8' }
+    })
+  }
+
   return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+    headers: { 'content-type': 'text/plain' }
   })
 }
